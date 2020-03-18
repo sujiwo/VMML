a808097602a525898866c653a8f8b414caece704
Adi Sujiwo <sujiwo@gmail.com>
2020-03-18 09:44:16 +0900

    Compilation for imagelidar_projection

--- vmml/vision_mapper/nodes/imagelidar_projection.cpp
+++ vmml/vision_mapper/nodes/imagelidar_projection.cpp
@@ -6,11 +6,23 @@
  */
 
 #include "ProgramOptions.h"
+#include "RVizConnector.h"
+
+
+using namespace Vmml;
+using namespace Vmml::Mapper;
 
 
 int main(int argc, char *argv[])
 {
+	ProgramOptions projOptions;
+	RVizConnector rosConn(argc, argv, "lidar_projection");
 
+	projOptions.parseCommandLineArgs(argc, argv);
+
+	auto lidarBag = projOptions.getLidarScanBag();
+	auto imageBag = projOptions.getImageBag();
+	auto &imagePipe = projOptions.getImagePipeline();
 
 	return 0;
 }
