7bda57efe89ee93880e0b2ba774c2fc5aee0f84b
Adi Sujiwo <sujiwo@gmail.com>
2020-03-31 23:04:10 +0900

    demo compilable

--- vmml_visualization/nodes/vosl_demo.cpp
+++ vmml_visualization/nodes/vosl_demo.cpp
@@ -9,7 +9,11 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include "yaml-cpp/yaml.h"
+
 #include "openvslam/system.h"
+#include "openvslam/config.h"
+
 #include "vmml/Pose.h"
 #include "vmml/Trajectory.h"
 #include "ProgramOptions.h"
@@ -17,6 +21,37 @@
 
 
 using namespace std;
+
+
+YAML::Node createDummyConfig(const Vmml::Mapper::ProgramOptions &po, float resample=-1)
+{
+	YAML::Node vsConf;
+
+	auto cameraConf = po.getWorkingCameraParameter();
+	vsConf["Camera.name"] = "Camera0";
+	vsConf["Camera.setup"] = "monocular";
+	vsConf["Camera.model"] = "perspective";
+	vsConf["Camera.fx"] = cameraConf.fx;
+	vsConf["Camera.fy"] = cameraConf.fy;
+	vsConf["Camera.cx"] = cameraConf.cx;
+	vsConf["Camera.cy"] = cameraConf.cy;
+	vsConf["Camera.k1"] = cameraConf.distortionCoeffs[0];
+	vsConf["Camera.k2"] = cameraConf.distortionCoeffs[1];
+	vsConf["Camera.p1"] = cameraConf.distortionCoeffs[2];
+	vsConf["Camera.p2"] = cameraConf.distortionCoeffs[3];
+	vsConf["Camera.p3"] = cameraConf.distortionCoeffs[4];
+	vsConf["Camera.fps"] = resample;
+	vsConf["Camera.cols"] = cameraConf.width;
+	vsConf["Camera.rows"] = cameraConf.height;
+	vsConf["Feature.max_num_keypoints"] = 2000;
+	vsConf["Feature.scale_factor"] = 1.2;
+	vsConf["Feature.num_levels"] = 8;
+	vsConf["Feature.ini_fast_threshold"] = 20;
+	vsConf["Feature.min_fast_threshold"] = 7;
+	vsConf["Camera.color_order"] = "BGR";
+
+	return vsConf;
+}
 
 
 int main(int argc, char *argv[])
@@ -36,6 +71,22 @@ int main(int argc, char *argv[])
 	auto startTime = vsoProg.get<float>("start-time");
 	auto stopTime = vsoProg.get<float>("stop-time");
 	auto resample = vsoProg.get<float>("resample");
+
+	if (resample==0)
+		resample = imageBag->hz();
+	imageBag->setTimeConstraint(startTime, stopTime);
+	RandomAccessBag::DesampledMessageList targetFrameId;
+	imageBag->desample(resample, targetFrameId);
+
+	Vmml::Mapper::RVizConnector rosConn(argc, argv, "vosl_demo");
+	rosConn.setImageTopicName("openvslam");
+
+	auto dummyYaml = createDummyConfig(vsoProg, resample);
+	auto slamConfig = make_shared<openvslam::config>(dummyYaml);
+
+	// Slam dunk
+	openvslam::system SlamDunk (slamConfig, vocPath);
+	SlamDunk.startup();
 
 	return 0;
 }
