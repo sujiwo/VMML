f2348ef270a357f0f8729d0669ce1864bf235c59
Adi Sujiwo <sujiwo@gmail.com>
2020-09-23 13:26:21 +0900

    amburadul

---
+++ place_recognizer/include/IncrementalBoW.h
@@ -0,0 +1,22 @@
+/*
+ * IncrementalBoW.h
+ *
+ *  Created on: Sep 23, 2020
+ *      Author: sujiwo
+ */
+
+#ifndef PLACE_RECOGNIZER_INCREMENTALBOW_H_
+#define PLACE_RECOGNIZER_INCREMENTALBOW_H_
+
+namespace PlaceRecognizer {
+
+class IncrementalBoW
+{
+public:
+	IncrementalBoW();
+	virtual ~IncrementalBoW();
+};
+
+} /* namespace PlaceRecognizer */
+
+#endif /* PLACE_RECOGNIZER_INCREMENTALBOW_H_ */
---
+++ place_recognizer/lib/IncrementalBoW.cpp
@@ -0,0 +1,21 @@
+/*
+ * IncrementalBoW.cpp
+ *
+ *  Created on: Sep 23, 2020
+ *      Author: sujiwo
+ */
+
+#include "/home/sujiwo/VmmlWorkspace/src/place_recognizer/include/IncrementalBoW.h"
+
+namespace PlaceRecognizer {
+
+IncrementalBoW::IncrementalBoW() {
+	// TODO Auto-generated constructor stub
+
+}
+
+IncrementalBoW::~IncrementalBoW() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace PlaceRecognizer */
