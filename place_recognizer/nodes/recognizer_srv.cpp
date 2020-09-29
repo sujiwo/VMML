052b671e4230ac52d033dc3e4e9feb37253fe2bc
Adi Sujiwo <sujiwo@gmail.com>
2020-09-29 14:12:28 +0900

    BoW Recognizer

--- place_recognizer/nodes/recognizer_srv.cpp
+++ place_recognizer/nodes/recognizer_srv.cpp
@@ -15,24 +15,83 @@
 #include "place_recognizer/place_recognizer.h"
 
 
+using namespace std;
+
+
 class RecognizerService
 {
 public:
-RecognizerService(ros::NodeHandle &n)
+RecognizerService(ros::NodeHandle &node, PrgOpt::ProgramOption &opt)
 {
+	binFeats = cv::ORB::create(
+			opt.get<int>("numfeats", 6000),
+			1.2,
+			8,
+			31,
+			0,
+			2,
+			cv::ORB::HARRIS_SCORE,
+			31,
+			10);
+	imagedb.loadFromDisk(opt.get<string>("mapfile", ""));
+	placeRecognSrv = node.advertiseService("place_recognizer", &RecognizerService::service, this);
+	cout << "Ready\n";
+}
 
+bool service(
+	place_recognizer::place_recognizer::Request &request,
+	place_recognizer::place_recognizer::Response &response)
+{
+	auto image = cv_bridge::toCvShare(request.input, nullptr, "bgr8")->image;
+	std::vector<cv::KeyPoint> kpList;
+	cv::Mat descriptors;
+	binFeats->detectAndCompute(image, cv::Mat(), kpList, descriptors, false);
+
+	vector<vector<cv::DMatch>> featureMatches;
+	imagedb.searchDescriptors(descriptors, featureMatches, 2, 32);
+	// Filter matches according to ratio test
+	vector<cv::DMatch> realMatches;
+	for (uint m=0; m<featureMatches.size(); m++) {
+		if (featureMatches[m][0].distance < featureMatches[m][1].distance * 0.65)
+			realMatches.push_back(featureMatches[m][0]);
+	}
+
+	vector<PlaceRecognizer::ImageMatch> imageMatches;
+	imagedb.searchImages(descriptors, realMatches, imageMatches);
+	response.keyframeId.clear();
+	for (int i=0; i<min(15, (int)imageMatches.size()); i++) {
+		response.keyframeId.push_back(imageMatches[i].image_id);
+	}
+
+	return true;
+}
+
+static
+PrgOpt::ProgramOption
+prepare_options()
+{
+	PrgOpt::ProgramOption opts;
+	opts.addSimpleOptions("mapfile", "Map file input path");
+	opts.addSimpleOptions<int>("numfeats", "Number of features from single image");
+	return opts;
 }
 
 private:
+	PlaceRecognizer::IncrementalBoW imagedb;
+	ros::ServiceServer placeRecognSrv;
+	cv::Ptr<cv::FeatureDetector> binFeats;
 };
 
 
 int main(int argc, char *argv[])
 {
+	auto opts = RecognizerService::prepare_options();
+	opts.parseCommandLineArgs(argc, argv);
+
 	ros::init(argc, argv, "place_recognizer");
 	ros::NodeHandle node;
-	RecognizerService srv(node);
 
+	RecognizerService srv(node, opts);
 	ros::spin();
 
 	return 0;
